a3613696060ba2229e7bbd5a9e63e702044ab031
IgorVini25 <igvinps2006@gmail.com>
2021-08-18 16:28:03 -0300

    :sparkles: The Project

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
+<div align="center">
+
 # Realtime-Clock
-A Simple Realtime Clock.
+
+## A Simple Realtime Clock.
+
+<br>
+<img src="src/readme.png" width="70%">
+
+</div>
+
+---
+
+## 🚀 Technologies
+
+- HTML
+- CSS
+- JavaScript
+
+---
+
+<h2> 🔖 License <h2>
+
+<h3>
+
+The Project license are MIT. See [LICENSE](LICENSE.md) file for more details.
+
+</h3>
---
+++ src/script.js
@@ -0,0 +1,52 @@
+const $ = document.querySelector.bind(document)
+
+const date = $('#date')
+const hour = $('#hour')
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+requestAnimationFrame(atualizer)
+
+async function atualizer() {
+  const ano = new Date()
+
+  const anoData = {
+    day: ano.getDate(),
+    month: ano.getMonth() + 1,
+    year: ano.getFullYear()
+  }
+
+  if (anoData.day < 10) {
+    anoData.day = '0' + `${anoData.day}`
+  }
+  if (anoData.month < 10) {
+    anoData.month = '0' + `${anoData.month}`
+  }
+
+  const hora = new Date()
+
+  const horaData = {
+    hour: hora.getHours(),
+    minutes: hora.getMinutes(),
+    seconds: hora.getSeconds()
+  }
+
+  if (horaData.hour < 10) {
+    horaData.hour = '0' + `${horaData.hour}`
+  }
+  if (horaData.minutes < 10) {
+    horaData.minutes = '0' + `${horaData.minutes}`
+  }
+  if (horaData.seconds < 10) {
+    horaData.seconds = '0' + `${horaData.seconds}`
+  }
+
+  date.innerText = `${anoData.day} / ${anoData.month} / ${anoData.year} `
+  hour.innerText = `${horaData.hour} : ${horaData.minutes} : ${horaData.seconds}`
+
+  await sleep(100)
+
+  requestAnimationFrame(atualizer)
+}
